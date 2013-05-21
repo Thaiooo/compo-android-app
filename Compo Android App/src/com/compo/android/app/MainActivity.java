44505c37abf615b3821c24274c5b342314400494
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2013-05-21 21:54:46 +0000

    Failed commit: Default

--- Compo Android App/src/com/compo/android/app/MainActivity.java
+++ Compo Android App/src/com/compo/android/app/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+	setContentView(R.layout.activity_main_small);
 
 	_userCredit = (TextView) findViewById(R.id.user_credit);
 	_userPoint = (TextView) findViewById(R.id.user_point);
