e74a1e7b0cdb437ee1726251fec0887e640be93f
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2013-05-23 11:37:37 +0000

    Failed commit: DefaultFailed commit: Default

--- trunk/Compo Android App/src/com/compo/android/app/StoreActivity.java
+++ trunk/Compo Android App/src/com/compo/android/app/StoreActivity.java
@@ -3,6 +3,7 @@ package com.compo.android.app;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,25 +14,23 @@ public class StoreActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	// requestWindowFeature(Window.FEATURE_NO_TITLE);
-	setContentView(R.layout.activity_store);
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_store);
 
-	System.out.println("Icic");
+        if (_font == null) {
+            _font = Typeface.createFromAsset(getAssets(), "MyLuckyPenny.ttf");
+        }
 
-	if (_font == null) {
-	    _font = Typeface.createFromAsset(getAssets(), "MyLuckyPenny.ttf");
-	}
+        Button buttonCredit25 = (Button) findViewById(R.id.button_credit_25);
+        Button buttonCredit100 = (Button) findViewById(R.id.button_credit_100);
+        Button buttonCredit500 = (Button) findViewById(R.id.button_credit_500);
+        buttonCredit25.setTypeface(_font);
+        buttonCredit100.setTypeface(_font);
+        buttonCredit500.setTypeface(_font);
 
-	Button buttonCredit25 = (Button) findViewById(R.id.button_credit_25);
-	Button buttonCredit100 = (Button) findViewById(R.id.button_credit_100);
-	Button buttonCredit500 = (Button) findViewById(R.id.button_credit_500);
-	buttonCredit25.setTypeface(_font);
-	buttonCredit100.setTypeface(_font);
-	buttonCredit500.setTypeface(_font);
-
-	_title = (TextView) findViewById(R.id.store_title);
-	_title.setTypeface(_font);
+        _title = (TextView) findViewById(R.id.store_title);
+        _title.setTypeface(_font);
 
     }
 
