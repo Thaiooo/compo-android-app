38b45f77f7af44a6bd03ecfb361c8131bd68a505
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2013-05-23 11:37:37 +0000

    Failed commit: DefaultFailed commit: Default

--- Compo Android App/src/com/compo/android/app/MainActivity.java
+++ Compo Android App/src/com/compo/android/app/MainActivity.java
@@ -1,7 +1,5 @@
 package com.compo.android.app;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.database.SQLException;
@@ -12,11 +10,12 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.compo.android.app.dao.DataBaseHelper;
 import com.compo.android.app.dao.UserDao;
 import com.compo.android.app.model.User;
 import com.compo.android.app.utils.UserFactory;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
@@ -29,86 +28,87 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	_userCredit = (TextView) findViewById(R.id.user_credit);
-	_userPoint = (TextView) findViewById(R.id.user_point);
-	_titlePart1 = (TextView) findViewById(R.id.titre_part1);
-	_titlePart2 = (TextView) findViewById(R.id.titre_part2);
+        _userCredit = (TextView) findViewById(R.id.user_credit);
+        _userPoint = (TextView) findViewById(R.id.user_point);
+        _titlePart1 = (TextView) findViewById(R.id.titre_part1);
+        _titlePart2 = (TextView) findViewById(R.id.titre_part2);
 
-	Button buttonPlay = (Button) findViewById(R.id.button_play);
-	Button buttonStore = (Button) findViewById(R.id.button_store);
-	Button buttonSetting = (Button) findViewById(R.id.button_setting);
+        Button buttonPlay = (Button) findViewById(R.id.button_play);
+        Button buttonStore = (Button) findViewById(R.id.button_store);
+        Button buttonSetting = (Button) findViewById(R.id.button_setting);
 
-	if (_font == null) {
-	    _font = Typeface.createFromAsset(getAssets(), "MyLuckyPenny.ttf");
-	}
-	buttonPlay.setTypeface(_font);
-	buttonStore.setTypeface(_font);
-	buttonSetting.setTypeface(_font);
+        if (_font == null) {
+            _font = Typeface.createFromAsset(getAssets(), "MyLuckyPenny.ttf");
+        }
+        buttonPlay.setTypeface(_font);
+        buttonStore.setTypeface(_font);
+        buttonSetting.setTypeface(_font);
 
-	if (_fontTitle == null) {
-	    // Eraser.ttf
-	    // HVD_Peace.ttf
-	    _fontTitle = Typeface.createFromAsset(getAssets(), "Eraser.ttf");
-	}
-	_titlePart1.setTypeface(_fontTitle);
-	_titlePart2.setTypeface(_fontTitle);
 
-	DataBaseHelper myDbHelper = new DataBaseHelper(this);
+        if (_fontTitle == null) {
+            // Eraser.ttf
+            // HVD_Peace.ttf
+            _fontTitle = Typeface.createFromAsset(getAssets(), "Eraser.ttf");
+        }
+        _titlePart1.setTypeface(_fontTitle);
+        _titlePart2.setTypeface(_fontTitle);
 
-	try {
-	    myDbHelper.createDataBase();
-	} catch (IOException ioe) {
-	    throw new Error("Unable to create database");
-	}
+        DataBaseHelper myDbHelper = new DataBaseHelper(this);
 
-	try {
-	    myDbHelper.openDataBase();
-	} catch (SQLException sqle) {
-	    throw sqle;
-	}
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
 
-	new LoadUserTask().execute();
+        try {
+            myDbHelper.openDataBase();
+        } catch (SQLException sqle) {
+            throw sqle;
+        }
+
+        new LoadUserTask().execute();
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	return false;
+        return false;
     }
 
     public void play(View view) {
-	Intent intent = new Intent(MainActivity.this, SelectThemeActivity.class);
-	startActivity(intent);
-	overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        Intent intent = new Intent(MainActivity.this, SelectThemeActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
     public void store(View view) {
-	Intent intent = new Intent(MainActivity.this, StoreActivity.class);
-	startActivity(intent);
+        Intent intent = new Intent(MainActivity.this, StoreActivity.class);
+        startActivity(intent);
     }
 
     public void setting(View view) {
-	Intent intent = new Intent(MainActivity.this, SettingActivity.class);
-	startActivity(intent);
+        Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+        startActivity(intent);
     }
 
     private class LoadUserTask extends AsyncTask<Void, Void, User> {
-	@Override
-	protected User doInBackground(Void... params) {
-	    UserDao dao = new UserDao(MainActivity.this);
-	    User u = dao.getUser();
-	    UserFactory.getInstance().setUser(u);
-	    return u;
-	}
+        @Override
+        protected User doInBackground(Void... params) {
+            UserDao dao = new UserDao(MainActivity.this);
+            User u = dao.getUser();
+            UserFactory.getInstance().setUser(u);
+            return u;
+        }
 
-	@Override
-	protected void onPostExecute(User anUser) {
-	    _userCredit.setText(anUser.getCredit() + "");
-	    _userPoint.setText(anUser.getPoint() + " pts");
-	}
+        @Override
+        protected void onPostExecute(User anUser) {
+            _userCredit.setText(anUser.getCredit() + "");
+            _userPoint.setText(anUser.getPoint() + " pts");
+        }
     }
 
 }
