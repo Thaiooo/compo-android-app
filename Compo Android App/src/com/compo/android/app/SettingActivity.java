13f51d89272849e75466417107f010d7a509da51
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2013-05-30 22:00:07 +0000

    Failed commit: DefaultFailed commit: Default

--- Compo Android App/src/com/compo/android/app/SettingActivity.java
+++ Compo Android App/src/com/compo/android/app/SettingActivity.java
@@ -1,16 +1,37 @@
 package com.compo.android.app;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class SettingActivity extends Activity {
 
+    private static Typeface _font;
+    private TextView _title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	requestWindowFeature(Window.FEATURE_NO_TITLE);
-	setContentView(R.layout.activity_setting);
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_setting);
+
+        if (_font == null) {
+            _font = Typeface.createFromAsset(getAssets(), "MyLuckyPenny.ttf");
+        }
+
+        _title = (TextView) findViewById(R.id.store_title);
+        _title.setTypeface(_font);
+    }
+
+    public void changeLanguage(View view) {
+        System.out.println("Change language");
+    }
+
+    public void changeSound(View view) {
+        System.out.println("Change sound");
     }
 
 }
