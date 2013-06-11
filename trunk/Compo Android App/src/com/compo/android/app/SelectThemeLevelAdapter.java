719ca36b8e315be98214797cea90e3b33bc7bfd3
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2013-06-11 21:34:01 +0000

    Failed commit: DefaultFailed commit: Default

--- trunk/Compo Android App/src/com/compo/android/app/SelectThemeLevelAdapter.java
+++ trunk/Compo Android App/src/com/compo/android/app/SelectThemeLevelAdapter.java
@@ -1,35 +1,35 @@
 package com.compo.android.app;
 
-import java.util.List;
-
-import com.compo.android.app.model.Theme;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.compo.android.app.model.Theme;
+
+import java.util.List;
 
 public class SelectThemeLevelAdapter extends FragmentStatePagerAdapter {
 
     private List<Theme> _themes;
 
     public SelectThemeLevelAdapter(FragmentManager fm, List<Theme> aThemes) {
-	super(fm);
-	_themes = aThemes;
+        super(fm);
+        _themes = aThemes;
     }
 
     @Override
     public Fragment getItem(int i) {
-	Fragment fragment = new ThemeLevelFragment();
-	Bundle args = new Bundle();
-	args.putSerializable(ThemeLevelFragment.EXTRA_MESSAGE_ARG, _themes.get(i));
-	fragment.setArguments(args);
-	return fragment;
+        Fragment fragment = new ThemeLevelFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ThemeLevelFragment.EXTRA_MESSAGE_ARG, _themes.get(i));
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public int getCount() {
-	return _themes.size();
+        return _themes.size();
     }
 
 }
