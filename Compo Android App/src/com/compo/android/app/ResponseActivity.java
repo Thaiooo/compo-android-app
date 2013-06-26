316003e5c53587fec3a37c0eb82248c91c7ab9a4
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2013-06-26 20:12:50 +0000

    Failed commit: DefaultFailed commit: Default

--- Compo Android App/src/com/compo/android/app/ResponseActivity.java
+++ Compo Android App/src/com/compo/android/app/ResponseActivity.java
@@ -1,14 +1,22 @@
 package com.compo.android.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 
-public class ResponseActivity extends Activity {
+import com.compo.android.app.model.QuizzPlayer;
 
+public class ResponseActivity extends Activity {
+    public static final String EXTRA_MESSAGE_ARG = "com.compo.android.app.ResponseActivity.MESSAGE.ARG";
+    private static final String TAG = ResponseActivity.class.getName();
     private static Typeface _font;
+    private EditText edit;
+    private QuizzPlayer currentQuizz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,11 @@ public class ResponseActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_response);
 
-        EditText edit = (EditText) findViewById(R.id.edit_response);
+        Intent intent = getIntent();
+        currentQuizz = (QuizzPlayer) intent.getSerializableExtra(EXTRA_MESSAGE_ARG);
+
+        edit = (EditText) findViewById(R.id.edit_response);
+        edit.setText(currentQuizz.getPlayer().getName());
 
         if (_font == null) {
             _font = Typeface.createFromAsset(getAssets(), "Eraser.ttf");
@@ -24,4 +36,18 @@ public class ResponseActivity extends Activity {
         edit.setTypeface(_font);
     }
 
+    public void check(View view) {
+        String response = edit.getText().toString().trim();
+        if (response.equals(currentQuizz.getPlayer().getName())) {
+            currentQuizz.setDiscovered(true);
+            Intent newIntent = new Intent();
+            newIntent.putExtra("Selected", "Yess");
+            setResult(RESULT_OK, newIntent);
+            finish();
+        } else {
+            Log.v(TAG, "Nooo");
+        }
+
+
+    }
 }
