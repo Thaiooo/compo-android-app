882f7a5c56e22a1d699123adb2f639bc6c9cf254
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@4cce5132-b2d8-f249-2bf4-5563aadda1f6>
2014-04-06 19:55:37 +0000

    Version IHM 1

--- Compo Android Admin/compo_manager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from compo_manager.models import Theme, Pack, Team, Match, Player
-  
-admin.site.register(Theme)
-admin.site.register(Pack)
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(Match)
